c9227771169b308d7537e9eb051f7297ae3cd4f0
C <C@C>
2019-12-16 17:07:58 +0530

    Fv

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        DbEntities mydb = new DbEntities();
+
+
+        public ActionResult Home()
+        {
+            return View("home", mydb.Employees.ToList());
+        }
+
+       public ActionResult Edit(int id)
+        {
+            Employee emp = mydb.Employees.Find(id);
+            return View("edit", emp);
+        }
+        public ActionResult AfterEdit(Employee emp)
+        {
+            Employee oldEmp = mydb.Employees.Find(emp.Id);
+            oldEmp.Name = emp.Name;
+            oldEmp.Address = emp.Address;
+            return View("Home");
+
+        }
+
+    }
+}
